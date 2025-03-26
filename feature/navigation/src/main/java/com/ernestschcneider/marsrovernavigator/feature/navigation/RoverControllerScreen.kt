fb614d75459dcb3d3fd4b2628356f33b1cc4dd2c
ernest75 <ernestschneider@gmail.com>
2025-03-26 12:19:32 +0100

    Pulled out RoverControlPanel in its on file

--- feature/navigation/src/main/java/com/ernestschcneider/marsrovernavigator/feature/navigation/RoverControllerScreen.kt
+++ feature/navigation/src/main/java/com/ernestschcneider/marsrovernavigator/feature/navigation/RoverControllerScreen.kt
@@ -1,17 +1,10 @@
 package com.ernestschcneider.marsrovernavigator.feature.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -19,12 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.ernestschcneider.marsrovernavigator.feature.navigation.view.MarsPlateau
+import com.ernestschcneider.marsrovernavigator.feature.navigation.view.RoverControlPanel
 import com.ernestschcneider.marsrovernavigator.view.R
 import com.ernestschcneider.marsrovernavigator.view.ui.theme.MarsRoverNavigatorTheme
 
@@ -57,27 +49,7 @@ private fun RoverControllerScreenContent() {
                 }
 
                 item {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        Button(
-                            onClick = { }
-                        ) {
-                            Text(stringResource(R.string.left_command))
-                        }
-                        Button(
-                            onClick = { }
-                        ) {
-                            Text(stringResource(R.string.move_command))
-                        }
-                        Button(
-                            onClick = { }
-                        ) {
-                            Text(
-                                text = stringResource(R.string.right_command)
-                            )
-                        }
-                    }
+                    RoverControlPanel()
                 }
 
                 item {
---
+++ feature/navigation/src/main/java/com/ernestschcneider/marsrovernavigator/feature/navigation/view/RoverControlPanel.kt
@@ -0,0 +1,54 @@
+package com.ernestschcneider.marsrovernavigator.feature.navigation.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.ernestschcneider.marsrovernavigator.feature.navigation.RoverControllerScreen
+import com.ernestschcneider.marsrovernavigator.view.R
+import com.ernestschcneider.marsrovernavigator.view.ui.theme.MarsRoverNavigatorTheme
+
+@Composable
+fun RoverControlPanel() {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Button(
+            onClick = { }
+        ) {
+            Text(stringResource(R.string.left_command))
+        }
+        Button(
+            onClick = { }
+        ) {
+            Text(stringResource(R.string.move_command))
+        }
+        Button(
+            onClick = { }
+        ) {
+            Text(
+                text = stringResource(R.string.right_command)
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun PreviewRoverControlPanel() {
+    MarsRoverNavigatorTheme {
+        Box(
+            modifier = Modifier.background(MaterialTheme.colorScheme.background)
+        ) {
+            RoverControlPanel()
+        }
+    }
+}
