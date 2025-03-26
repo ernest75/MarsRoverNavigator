f1003015bd172378f891017a2d5123813d7f5721
ernest75 <ernestschneider@gmail.com>
2025-03-26 18:17:02 +0100

    Added blank lines at the end of the file

--- feature/navigation/src/main/java/com/ernestschcneider/marsrovernavigator/feature/navigation/RoverControllerScreen.kt
+++ feature/navigation/src/main/java/com/ernestschcneider/marsrovernavigator/feature/navigation/RoverControllerScreen.kt
@@ -5,19 +5,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.ernestschcneider.marsrovernavigator.feature.navigation.view.MarsPlateau
 import com.ernestschcneider.marsrovernavigator.feature.navigation.view.RoverControlPanel
 import com.ernestschcneider.marsrovernavigator.feature.navigation.view.Title
-import com.ernestschcneider.marsrovernavigator.view.R
 import com.ernestschcneider.marsrovernavigator.view.ui.theme.MarsRoverNavigatorTheme
 
 
